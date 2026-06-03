46162812131c9c70aa549698541ae40efbd025b2
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-03 09:27:09 +0530

    Initial Testing of the github

---
+++ src/main/java/com/example/affordMedical/AffordMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.affordMedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffordMedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffordMedicalApplication.class, args);
+	}
+
+}
